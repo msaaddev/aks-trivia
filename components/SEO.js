6c76329ed8f239eab48c87710c042385cbb5285a
msaaddev <mrsaadirfan@gmail.com>
2021-04-23 21:00:32 +0500

    👌 IMPROVE: formatting

--- components/SEO.js
+++ components/SEO.js
@@ -1,26 +1,32 @@
-import Head from 'next/head';
+import Head from "next/head";
 
 const SEO = () => {
-    return (
-        <Head>
-            <title>AKS – Trivia</title>
-            <link rel='icon' href='/favicon.ico' />
-            <link rel='preconnect' href='https://fonts.gstatic.com' />
-            <link
-                href='https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700&display=swap'
-                rel='stylesheet'
-            />
-            <link
-                rel='stylesheet'
-                href='https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap'
-            />
-            <link rel='stylesheet' href='https://fonts.googleapis.com/icon?family=Material+Icons' />
-            <meta charset='UTF-8' />
-            <meta name='viewport' content='width=device-width, initial-scale=1.0' />
-            <meta name='description' content='AKS Photography Society Trivia Night' />
-            <meta name='keywords' content='aks, aks photography society, photography society' />
-        </Head>
-    );
+	return (
+		<Head>
+			<title>AKS – Trivia</title>
+			<link rel="icon" href="/favicon.ico" />
+			<link rel="preconnect" href="https://fonts.gstatic.com" />
+			<link
+				href="https://fonts.googleapis.com/css2?family=Raleway:wght@200;300;400;500;600;700&display=swap"
+				rel="stylesheet"
+			/>
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"
+			/>
+			<link
+				rel="stylesheet"
+				href="https://fonts.googleapis.com/icon?family=Material+Icons"
+			/>
+			<meta charset="UTF-8" />
+			<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+			<meta name="description" content="AKS Photography Society Trivia Night" />
+			<meta
+				name="keywords"
+				content="aks, aks photography society, photography society"
+			/>
+		</Head>
+	);
 };
 
 export default SEO;
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,11 @@
-import SEO from '../components/SEO';
-import Trivia from '../components/Trivia';
+import SEO from "../components/SEO";
+import Trivia from "../components/Trivia";
 
 export default function Home() {
-    return (
-        <>
-            <SEO />
-            <Trivia />
-        </>
-    );
+	return (
+		<>
+			<SEO />
+			<Trivia />
+		</>
+	);
 }
